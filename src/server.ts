4b663ee5a46690cd6d304af15aa51109f4e9edc1
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-03-26 09:59:07 +0000

    Listen to some more WSS events

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,32 @@
 import WebSocket from 'ws'
 
 const wss = new WebSocket.Server({ port: 8080 })
+wss.on('listening', function () {
+  console.log('Listening on ws://localhost:8080')
+})
 
-console.log('doing somethin!')
-
+let connectionID = 0
 wss.on('connection', function connection(ws) {
+  connectionID++
+
   ws.on('message', function incoming(message) {
     console.log('received: %s', message)
   })
 
-  ws.send('you connected :D')
+  const res = { info: `You're connected with ID ${connectionID} :D` }
+  ws.send(JSON.stringify(res))
+})
+
+wss.on('close', function closing() {
+  console.log('Bye 👋')
+})
+
+process.on('SIGINT', (sig) => {
+  console.log()
+  wss.close((err) => {
+    if (!err) process.exit(0)
+
+    console.log(err.stack)
+    process.exit(1)
+  })
 })
